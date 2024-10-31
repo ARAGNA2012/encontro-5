07711cf002377262e8871f7d7da2382e05c0000d
ARAGNA2012 <aragna2012@gmail.com>
2024-10-31 11:09:00 -0300

    Add files via upload

---
+++ encontro_5.ino
@@ -0,0 +1,40 @@
+int ledverde=11;
+int ledamarelo=10;
+int ledlaranja=9;
+int ledvermelho=6;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ledverde,OUTPUT);
+pinMode(ledamarelo,OUTPUT);
+pinMode(ledlaranja,OUTPUT);
+pinMode(ledvermelho,OUTPUT);
+
+}
+
+void loop() {
+  int tempo=analogRead(A0);
+  Serial.println(tempo);
+  
+  digitalWrite(ledverde,HIGH);
+  delay(tempo);
+  digitalWrite(ledverde,LOW);
+  delay(tempo);
+
+  digitalWrite(ledamarelo,HIGH);
+  delay(tempo);
+  digitalWrite(ledamarelo,LOW);
+  delay(tempo);
+
+  digitalWrite(ledlaranja,HIGH);
+  delay(tempo);
+  digitalWrite(ledlaranja,LOW);
+  delay(tempo);
+
+  digitalWrite(ledvermelho,HIGH);
+  delay(tempo);
+  digitalWrite(ledvermelho,LOW);
+  delay(tempo);
+
+}
